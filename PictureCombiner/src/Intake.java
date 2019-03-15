8ec1785ac48cc5111befcc0bfc022235ca00b20b
tbehi <tbehi@Bali>
2019-03-15 17:35:19 -0600

    QS

--- PictureCombiner/src/Intake.java
+++ PictureCombiner/src/Intake.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.io.IOException;
 
@@ -24,10 +25,14 @@ public class Intake extends Thread
 		File[] files = folder.listFiles();
 		for(int i = 0; i < colors.length; i++)
 		{
-			MyBufferedImage bi = null;
+			BufferedImage bi = null;
 			try
 			{
 				bi = ImageIO.read(files[i]);
+				final int width = bi.getWidth(), height = bi.getHeight();
+				BufferedImage newBi = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+				newBi.createGraphics().drawImage(bi, 0, 0, width, height, null);
+				bi = newBi;
 			} 
 			catch (IOException e)
 			{
@@ -36,7 +41,8 @@ public class Intake extends Thread
 			
 			if(bi == null)
 				continue;
-			colors[i] = bi.getMeanColor();
+			//colors[i] = bi.getMeanColor();
+			getMeanColor(bi);
 		}
 	}
 	
@@ -54,5 +60,26 @@ public class Intake extends Thread
 		}
 		getInstance().folder = input;
 		return instance;
+	}
+	
+	public static void getMeanColor(BufferedImage bi)
+	{
+		if(bi.getType() != BufferedImage.TYPE_3BYTE_BGR) throw new IllegalArgumentException();
+		int rBucket = 0; int gBucket = 0; int bBucket = 0;
+		byte[] pixels = ((DataBufferByte)bi.getRaster().getDataBuffer()).getData();
+		for(int curPixelSet = 0; curPixelSet < pixels.length; curPixelSet += 3)
+		{
+			byte red = pixels[curPixelSet + 2];
+			System.out.println("Red: " + getByteString(red));
+			byte green = pixels[curPixelSet + 1];
+			System.out.println("Green: " + getByteString(green));
+			byte blue = pixels[curPixelSet];
+			System.out.println("Blue: " + getByteString(blue));
+		}
+	}
+	
+	public static String getByteString(byte input)
+	{
+		return String.format("%8s", Integer.toBinaryString(input & 0xFF)).replace(' ', '0');
 	}
 }
